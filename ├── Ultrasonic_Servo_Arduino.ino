3713c58ed03d29cba4750d2d1fcacaa35d3c574d
AdelAteeq <adel.a.ateeq@gmail.com>
2026-08-02 05:13:01 +0300

    Create ├── Ultrasonic_Servo_Arduino.ino

---
+++ ├── Ultrasonic_Servo_Arduino.ino
@@ -0,0 +1,49 @@
+#include <Servo.h>
+
+Servo myServo;
+
+const int trigPin = 9;
+const int echoPin = 10;
+const int servoPin = 6;
+const int ledPin = 7;
+
+long duration;
+float distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+
+  myServo.attach(servoPin);
+  myServo.write(0);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.0343 / 2;
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  if (distance <= 10) {
+    myServo.write(90);
+    digitalWrite(ledPin, HIGH);
+  } 
+  else {
+    myServo.write(0);
+    digitalWrite(ledPin, LOW);
+  }
+
+  delay(100);
+}
